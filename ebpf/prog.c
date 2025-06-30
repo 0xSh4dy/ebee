8f04cc534d9ec496848bf217018ff65acbb7189e
0xSh4dy <rakshitawasthi17@gmail.com>
2025-06-30 17:43:19 +0530

    Update handler

--- ebpf/prog.c
+++ ebpf/prog.c
@@ -1,10 +1,19 @@
+#include "vmlinux.h"
 #include <bpf/bpf_helpers.h>
-#include <linux/bpf.h>
-#include <linux/if_ether.h>
-#include <linux/if_packet.h>
-#include <linux/in.h>
-#include <linux/ip.h>
-#include <linux/ipv6.h>
+#define ETH_P_IP 0x0800
+
+struct ebee_event{
+  unsigned char payload[20];
+};
+
+struct{
+  __uint(type,BPF_MAP_TYPE_RINGBUF);
+  __uint(max_entries,1<<12);
+}ebee_map SEC(".maps");
+
+static inline unsigned short htons(unsigned short x) {
+  return (x << 8) | (x >> 8);
+}
 
 SEC("xdp")
 int check_packets(struct xdp_md *ctx) {
@@ -19,16 +28,44 @@ int check_packets(struct xdp_md *ctx) {
   }
   unsigned short h_proto = eth->h_proto;
 
-  if (h_proto == htons(ETH_P_IP)) {
-    void *ip_data = data + ip_header_offset;
-    struct iphdr *ip_header = ip_data;
-    
-    if ((void *)&ip_header[1] > data_end) {
-      return 0;
+  if (h_proto != htons(ETH_P_IP)) {
+    return XDP_PASS;
+  }
+  void *ip_data = data + ip_header_offset;
+  struct iphdr *ip_header = ip_data;
+
+  if ((void *)&ip_header[1] > data_end) {
+    return XDP_PASS;
+  }
+
+  if (ip_header->protocol != IPPROTO_UDP) {
+    return XDP_PASS;
+  }
+
+  struct udphdr *udp = (void *)ip_header + ip_header->ihl * 4;
+
+  if ((void *)(udp + 1) > data_end) {
+    return XDP_PASS;
+  }
+
+  unsigned short port = __builtin_bswap16(udp->dest);
+
+  bpf_printk("Received UDP packet on %d",port);
+  if (port != 8080) {
+    return XDP_PASS;
+  }
+  unsigned char *payload = (unsigned char *)(udp + 1);
+
+  if (payload + 20 <= (unsigned char *)data_end) {
+    struct ebee_event *evt = bpf_ringbuf_reserve(&ebee_map,sizeof(struct ebee_event),0);
+    if(!evt){
+      bpf_printk("failed to allocate memory for the ring buffer");
+      return XDP_PASS;
     }
-    if(ip_header->protocol == IPPROTO_UDP){
-        
-    }
+    __builtin_memcpy(evt->payload, payload, 20);
+    bpf_ringbuf_submit(evt,0);
   }
   return XDP_PASS;
 }
+
+char _license[] SEC("license") = "GPL";
--- src/main.cc
+++ src/main.cc
@@ -1,52 +1,116 @@
 #include "instructions.h"
-#include <iostream>
-#include <net/if.h>
 #include <bpf/bpf.h>
 #include <bpf/libbpf.h>
-#include <llvm/IR/LLVMContext.h>
+#include <csignal>
+#include <iostream>
 #include <llvm/IR/Function.h>
 #include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/LLVMContext.h>
+#include <net/if.h>
+#include <unistd.h>
 
-int main(int argc,char **argv){
-    // llvm::LLVMContext ctx;
-    // auto module = std::make_unique<llvm::Module>("shadie_module",ctx);
-    // ebee::Instruction *x = new ebee::Xor();
-    // x->build(ctx,module.get());
-    if(argc < 3){
-        fprintf(stderr, "Usage: %s <ifname> <bpf_obj_file>\n", argv[0]);
-        return 1;
-    }
-    const char *if_name = argv[1];
-    const char *obj_file = argv[2];
-    int ifindex = if_nametoindex(if_name);
+static int ifindex = 0;
+static int attach_flags = 0;
 
-    if(!ifindex){
-        perror("if_nametoindex");
-        return 1;
-    }
-    
-    int prog_fd;
-    struct bpf_object *obj;
-    struct bpf_program *prog;
+struct ebee_event {
+  unsigned char payload[20];
+};
 
-    obj = bpf_object__open(obj_file);
-    if(!obj){
-        std::cerr<<"failed to open the eBPF object file\n";
-        return 1;
+static void handle_signal(int sig) {
+  std::cout << "\nSignal received (" << sig << "), detaching XDP program...\n";
+  if (ifindex) {
+    bpf_xdp_detach(ifindex, attach_flags, NULL);
+  }
+  exit(0);
+}
+
+static int event_handler(void *ctx, void *data, size_t len) {
+    std::cout<<"Triggered event handler\n";
+  struct ebee_event *evt = (struct ebee_event*)data;
+  for(int i=0;i<20;i++){
+    printf("%02x ",evt->payload[i]);
+  }
+  fflush(stdout);
+  printf("\n");
+  return 0;
+}
+
+int main(int argc, char **argv) {
+  if (argc < 3) {
+    fprintf(stderr, "Usage: %s <ifname> <bpf_obj_file>\n", argv[0]);
+    return 1;
+  }
+
+  const char *if_name = argv[1];
+  const char *obj_file = argv[2];
+  ifindex = if_nametoindex(if_name);
+  attach_flags = 0;
+
+  if (!ifindex) {
+    perror("if_nametoindex");
+    return 1;
+  }
+
+  struct sigaction sa = {};
+  sa.sa_handler = handle_signal;
+  sigaction(SIGINT, &sa, NULL);
+  sigaction(SIGTERM, &sa, NULL);
+
+  struct bpf_object *obj;
+  struct bpf_program *prog;
+
+  obj = bpf_object__open(obj_file);
+  if (!obj) {
+    std::cerr << "failed to open the eBPF object file\n";
+    return 1;
+  }
+
+  if (bpf_object__load(obj)) {
+    perror("bpf_object__load");
+    std::cerr << "failed to load the eBPF program\n";
+    return 1;
+  }
+
+  prog = bpf_object__find_program_by_name(obj, "check_packets");
+  if (!prog) {
+    std::cerr << "failed to open the XDP program\n";
+    return 1;
+  }
+
+  int prog_fd = bpf_program__fd(prog);
+  if (prog_fd < 0) {
+    perror("bpf_program__fd");
+    std::cerr << "failed to retrieve the file descriptor of the eBPF program\n";
+    return 1;
+  }
+
+  if (bpf_xdp_attach(ifindex, prog_fd, attach_flags, NULL)) {
+    std::cerr << "failed to attach the program to network interface\n";
+    return 1;
+  }
+
+  int rb_fd = bpf_object__find_map_fd_by_name(obj, "ebee_map");
+  if (rb_fd < 0) {
+    perror("bpf_object__find_map_fd_by_name");
+    std::cerr << "failed to find the file descriptor of ebee_map\n";
+    return 1;
+  }
+
+  struct ring_buffer *rbuffer =
+      ring_buffer__new(rb_fd, event_handler, NULL, NULL);
+  if (!rbuffer) {
+    std::cerr << "failed to create the ring buffer\n";
+    return 1;
+  }
+
+  while (true) {
+    int e = ring_buffer__consume(rbuffer);
+    if (e < 0) {
+      std::cerr << "ring_buffer__consume error\n";
+      break;
     }
-    prog = bpf_object__next_program(obj,NULL);
-    if(!prog){
-        std::cerr<<"failed to open the XDP program\n";
-        return 1;
-    }
-    prog_fd = bpf_program__fd(prog);
-    if(prog_fd < 0){
-        std::cerr<<"failed to retrieve the file descriptor of the eBPF program\n";
-        return 1;
-    }
-    if(bpf_xdp_attach(ifindex,prog_fd,0,NULL)){
-        std::cerr<<"failed to attach the program to network interface\n";
-        return 1;
-    }
-    return 0;
+    sleep(1);
+  }
+
+  return 0;
 }
